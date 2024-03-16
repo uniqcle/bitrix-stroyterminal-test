d83ecd61c4b037714de1134be0baed3389302e08
Uniqcle <uniqcle@yandex.ru>
2024-03-16 13:48:59 +0700

    elements added

--- local/modules/uniqcle.test/include.php
+++ local/modules/uniqcle.test/include.php
@@ -1,1 +1,15 @@
 <?php
+//use \Bitrix\Main\Loader;
+//use \Uniqcle\IBlock\IBlockUniqcle;
+//
+//Loader::includeModule("uniqcle.test");
+//
+//IBlockUniqcle::test();
+
+
+CModule::AddAutoloadClasses(
+	"uniqcle.test",
+	array(
+		"Uniqcle\\IBlock\\IBlockUniqcle" => "lib/iblock.php",
+	)
+);
